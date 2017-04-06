8ba4d67c00cb7e31718e10c0611de4a2f23fb3d5
RindangRamadhan <rindangramadhan10@gmail.com>
2017-04-06 16:55:49 +0700

    Master Data Kategori Culture Experience
    Membuat Proses Edit Kategori
    Mebuat Proses Hapus Kategori

---
+++ resources/views/kategori/edit.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+    <main class="site-main page-spacing">
+        <!-- Page Banner -->
+        <div class="container-fluid page-banner about-banner">
+            <div class="container">
+                <h3>Edit Kategori</h3>
+                <ol class="breadcrumb">
+                    <li><a href="{{ url('/home')}}">Admin</a></li>
+                    <li class="">Kategori</li>
+                    <li class="active">Edit</li>
+                </ol>
+            </div>
+        </div><!-- Page Banner /- -->
+        
+                <div class="section-top-padding"></div>
+
+        <!-- Recommended Section -->
+        <div id="recommended-section" class="recommended-section container-fluid no-padding">
+            <!-- Container -->
+            <div class="container">
+               
+            {!! Form::model($kategori, ['url' => route('kategori.update', $kategori->id),
+            'method' => 'put', 'files'=>'true', 'class'=>'form-horizontal']) !!}
+
+                    @include('kategori._form')
+
+            {!! Form::close() !!}
+                
+            </div><!-- Container /- -->
+            <div class="section-padding"></div>
+        </div><!-- Recommended Section /- -->
+        
+    </main>
+
+@endsection     
+
+@section('scripts')
+    <script type="text/javascript">
+        CKEDITOR.replace('deskripsi_kategori');
+    </script>
+@endsection
