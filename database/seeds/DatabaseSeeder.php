21abbc939641e9d416b97968fd90859a81273aaf
RindangRamadhan <rindangramadhan10@gmail.com>
2017-04-06 16:09:23 +0700

    Master Data Kategori Culture Experience
    - Membuat Proses Tambah Kategori
    - Menampilkan Data Kategori

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersSeeder::class);
         $this->call(DestinasiSeeder::class);
+        $this->call(KategoriSeeder::class);
     }
 }
