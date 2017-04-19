391eead5822ecb3ff7b1ee135e888b3d474e1c35
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2017-04-19 12:02:22 +0700

    Tambah Jumlah Orang jadi 15 di pencarian

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -86,7 +86,18 @@
                             '2' => '2',
                             '3' => '3',
                             '4' => '4',
-                            '5' => '5'],null,['class'=>'selectpicker','placeholder'=>'JUMLAH ORANG']) !!}
+                            '5' => '5',
+                            '6' => '6',
+                            '7' => '7',
+                            '8' => '8',
+                            '9' => '9',
+                            '10' => '10',
+                            '11' => '11',
+                            '12' => '12',
+                            '13' => '13',
+                            '14' => '14',
+                            '15' => '15',],null,['class'=>'selectpicker','placeholder'=>'JUMLAH ORANG']) !!}
+
 
                              {!! $errors->first('jumlah_orang', '<p class="help-block">:message</p>') !!}
 
