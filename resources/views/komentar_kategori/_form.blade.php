85ac278e7945812320719c763137bd160acd94d8
Ghazamalghifari <ghozam.alghifari96@gmail.com>
2017-04-28 10:54:23 +0700

    membatasi karakter komentar menjadi 500
    - komentar detail cultural
    - komentar detail homestay

--- resources/views/komentar_kategori/_form.blade.php
+++ resources/views/komentar_kategori/_form.blade.php
@@ -3,7 +3,7 @@
 						<h3>Komentar</h3>
 						<form class="row"> 				
 							<div class="form-group col-md-12">							
-								{!! Form::textarea('isi_komentar', null, ['class'=>'form-control', 'placeholder' => 'Tulisakan Komentar',]) !!}
+								{!! Form::textarea('isi_komentar', null, ['class'=>'form-control', 'placeholder' => 'Tulisakan Komentar',,'maxlength' => 500]) !!}
 							</div>
 
 							{!! Form::hidden('id_kategori', $value  = $detail_cultural->id, ['class'=>'form-control']) !!}
